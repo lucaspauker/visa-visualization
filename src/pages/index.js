e16d9cf27e71c2d6af53d395aa961a2358dd05ae
Lucas Pauker <lucas.pauker@gmail.com>
2022-06-29 21:50:05 -0700

    Add pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,15 @@
 import React from 'react';
 
-import Layout from '@common/Layout';
-import Navbar from '@common/Navbar';
-import LoginButton from '@common/LoginButton';
-
-import Header from '@sections/Header';
-import About from '@sections/About';
-import Brands from '@sections/Brands';
-import Team from '@sections/Team';
-import Faq from '@sections/Faq';
-import Footer from '@sections/Footer';
-
+import App from '../components/App';
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const IndexPage = () => (
   <Auth0Provider
     domain="dev-lcrt6eds.us.auth0.com"
     clientId="VTUyceSP3pRytyixJ6N3DuCFDb06ya0l"
-    redirectUri={window.location.origin}
+    redirectUri={"http://localhost:8000"}
   >
-    <Layout>
-      <Navbar />
-      <Header />
-      <About />
-      <Brands />
-      <Team />
-      <Footer />
-    </Layout>
+    <App />
   </Auth0Provider>
 );
 
